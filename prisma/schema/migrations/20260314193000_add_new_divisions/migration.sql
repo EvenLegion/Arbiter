810f2e6a5eab2285901d32cd61524c77803e89b4
Wyatt Barnes <me@wyatt.email>
2026-03-14 21:51:39 -1000

    fix migration file

--- prisma/schema/migrations/20260314193000_add_new_divisions/migration.sql
+++ prisma/schema/migrations/20260314193000_add_new_divisions/migration.sql
@@ -13,8 +13,8 @@ INSERT INTO "Division" (
 VALUES
 	('AFK', 'On Leave of Absence', 'SPECIAL', 'AFK', TRUE, NULL, NULL, NULL, NOW(), NOW()),
 	('TECHDEPT', 'Tech Department', 'STAFF', 'TECH', FALSE, NULL, NULL, NULL, NOW(), NOW()),
-	('SOL', 'Staff on Leave', 'STAFF', 'SOL', FALSE, NULL, NULL, NULL, NOW(), NOW())
-	('CMDN', 'Navy Commander', 'STAFF', 'CMD-N', FALSE, NULL, NULL, NULL, NOW(), NOW())
-	('CMDM', 'Marines Commander', 'STAFF', 'CMD-M', FALSE, NULL, NULL, NULL, NOW(), NOW())
+	('SOL', 'Staff on Leave', 'STAFF', 'SOL', FALSE, NULL, NULL, NULL, NOW(), NOW()),
+	('CMDN', 'Navy Commander', 'STAFF', 'CMD-N', FALSE, NULL, NULL, NULL, NOW(), NOW()),
+	('CMDM', 'Marines Commander', 'STAFF', 'CMD-M', FALSE, NULL, NULL, NULL, NOW(), NOW()),
 	('CMDS', 'Support Commander', 'STAFF', 'CMD-S', FALSE, NULL, NULL, NULL, NOW(), NOW())
 ON CONFLICT ("code") DO NOTHING;
